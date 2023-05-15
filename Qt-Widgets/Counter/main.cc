00d634fdb1f6c202df65417cc3526efccec7e745
dweller4085 <immbirj@gmail.com>
2023-05-15 13:25:54 +0700

    Began work on Counter

---
+++ Qt-Widgets/Counter/main.cc
@@ -0,0 +1,11 @@
+#include <QApplication>
+#include <QCoreApplication>
+#include <MKB/aliases.h>
+#include "mainwindow.hh"
+
+int main (int argc, char ** argv) {
+    QApplication app {argc, argv};
+    MainWindow window {};
+    window.show();
+    return app.exec();
+}
---
+++ Qt-Widgets/Counter/mainwindow.hh
@@ -0,0 +1,20 @@
+#ifndef MAINWINDOW_HH
+#define MAINWINDOW_HH
+
+#include <QObject>
+#include <QtGui>
+#include <QTextCodec>
+#include <QWidget>
+#include <QFrame>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QMessageBox>
+
+class MainWindow : public QWidget { Q_OBJECT
+    
+};
+
+#endif // MAINWINDOW_HH
