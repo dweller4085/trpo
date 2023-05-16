7d87e433cbfd742e50d5377edbc1e61e501c32f5
dweller4085 <immbirj@gmail.com>
2023-05-17 02:13:38 +0700

    Settled on the classes

--- Qt-Widgets/Moving/mainwindow.cc
+++ Qt-Widgets/Moving/mainwindow.cc
@@ -2,40 +2,10 @@
 #include <QHBoxLayout>
 #include "mainwindow.hh"
 
-MainWindow::MainWindow(QWidget * parent):
-    QWidget {parent},
+MainWindow::MainWindow():
+    scene {},
     codec {QTextCodec::codecForName("UTF-8")},
-    onesCounter {
-        new QLabel {codec->toUnicode("Cчет по 1"), this},
-        new Counter {0, 5, this}
-    },
-    fivesCounter {
-        new QLabel {codec->toUnicode("Cчет по 5"), this},
-        new Counter {this}
-    },
-    incrementButton {new QPushButton {"+1", this}},
-    exitButton {new QPushButton {codec->toUnicode("Выход"), this}}
+    exitButton {new QPushButton {this}}
 {
-    this->setWindowTitle(codec->toUnicode("Счетчик"));
-
-    QHBoxLayout * lables = new QHBoxLayout();
-    lables->addWidget(onesCounter.label);
-    lables->addWidget(fivesCounter.label);
-
-    QHBoxLayout * edits = new QHBoxLayout();
-    edits->addWidget(onesCounter.counter);
-    edits->addWidget(fivesCounter.counter);
-
-    QHBoxLayout * buttons = new QHBoxLayout();
-    buttons->addWidget(incrementButton);
-    buttons->addWidget(exitButton);
-
-    QVBoxLayout * mainLayout = new QVBoxLayout {this};
-    mainLayout->addLayout(lables);
-    mainLayout->addLayout(edits);
-    mainLayout->addLayout(buttons);
-
-    connect(incrementButton, &QPushButton::clicked, onesCounter.counter, &Counter::increment);
-    connect(onesCounter.counter, &Counter::tick, fivesCounter.counter, &Counter::increment);
-    connect(exitButton, &QPushButton::clicked, this, &QWidget::close);
+    auto layout
 }
--- Qt-Widgets/Moving/mainwindow.hh
+++ Qt-Widgets/Moving/mainwindow.hh
@@ -8,20 +8,59 @@
 #include <QLabel>
 #include <QLineEdit>
 #include <QPushButton>
-#include "counter.hh"
+#include <QPainter>
+#include <MKB/aliases.h>
+
+struct Vertex {
+    f32 x;
+    f32 y;
+};
+
+class Shape {
+protected:
+    QList<Vertex> vertices;
+
+public:
+    virtual void draw(QPainter& painter) = 0;
+    virtual ~Shape() = default;
+    void rotate(f32 angle);
+};
+
+class Line : public Shape {
+public:
+    Line(Vertex const points[2]);
+    virtual void draw(QPainter &painter) override;
+};
+
+class Rectangle : public Shape {
+public:
+    Rectangle(Vertex const points[4]);
+    virtual void draw(QPainter &painter) override;
+};
+
+class Scene : public QWidget { Q_OBJECT
+    Line line;
+    Rectangle rectangle;
+    s32 timerId;
+    f32 angle;
+
+protected:
+    void paintEvent(QPaintEvent * event) override;
+    void timerEvent(QTimerEvent * event) override;
+    void showEvent(QShowEvent * event) override;
+    void hideEvent(QHideEvent * event) override;
+
+public:
+    Scene(QWidget * parent);
+};
 
 class MainWindow : public QWidget { Q_OBJECT
-    QTextCodec * codec;
-    struct LabeledCounter {
-        QLabel * label;
-        Counter * counter;
-    } onesCounter, fivesCounter;
-    QPushButton * incrementButton;
+    Scene scene;
+    QTextCodec codec;
     QPushButton * exitButton;
 
 public:
-    MainWindow(QWidget * parent);
-    MainWindow(): MainWindow {nullptr} {}
+    MainWindow();
 };
 
 #endif // MAINWINDOW_HH
