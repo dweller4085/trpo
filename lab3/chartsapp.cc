17021da53c523b11f688b8b853e65b7f36a4980e
dweller4085 <immbirj@gmail.com>
2023-06-06 15:05:39 +0700

    Added stretch factors for FileView and ChartView

--- lab3/chartsapp.cc
+++ lab3/chartsapp.cc
@@ -10,6 +10,10 @@ ChartsApp::ChartsApp() {
     splitter->addWidget(fileView);
     splitter->addWidget(chartView);
 
+    //this does work
+    splitter->setStretchFactor(0, 2);
+    splitter->setStretchFactor(1, 5);
+
     setCentralWidget(splitter);
 
     setMinimumHeight(360);
